c847480dacc581c5b39906c0c195d644ec978420
Rusted Wizard <chensike89@outlook.com>
2019-06-04 17:40:37 -0700

    Code improvement. Now, the sorting of all nodes according to its weight is eliminated. Now, only a simple traverse of the dictionary is used to pick out smallest node.

--- GraphCollection/DijiktraNode.cs
+++ GraphCollection/DijiktraNode.cs
@@ -2,7 +2,7 @@
 
 namespace GraphCollection
 {
-    public class DijiktraNode<T> : Node<T>, IComparable<DijiktraNode<T>> where T : class
+    public class DijiktraNode<T> : Node<T>
     {
         //when WeightSet == false
         //the filed _weight is considered to be equal to infinity.
@@ -29,30 +29,6 @@ namespace GraphCollection
         public DijiktraNode(T value) : base(value)
         {
             WeightSet = false;
-        }
-
-        //IComparable interface implementation
-        public int CompareTo(DijiktraNode<T> value)
-        {
-            //if both nodes' weight set to infinity
-            //then the weight is the same
-            if (!WeightSet && !value.WeightSet)
-            {
-                return 0;
-            }
-            //if one node's weight set to infinity
-            //then that node is bigger
-            if (WeightSet && !value.WeightSet)
-            {
-                return -1;
-            }
-            if (!WeightSet && value.WeightSet)
-            {
-                return 1;
-            }
-            //if both nodes' value is set
-            //then compare the weight as usual
-            return (Weight - value.Weight);
         }
     }
 }
